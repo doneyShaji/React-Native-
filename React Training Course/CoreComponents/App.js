37499be6fddb5201f764c5f2a79a45ca84986003
doneyShaji <doney.shaji@people10.com>
2024-08-13 19:51:46 +0530

    React Native Introduction

---
+++ React Training Course/CoreComponents/App.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function App() {
+  return <View style={{ flex: 1, backgroundColor: "plum" }}>
+    <View style={{ width: 200, height: 200, backgroundColor: "lightblue"}}></View>
+  </View>;
+}
